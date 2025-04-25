b16d88bcf4098fff422ae88cf804a3939eb9a915
QuynhAnNguyen1411 <quynhann11021997@gmail.com>
2025-04-25 22:24:20 +0700

    update and create api update  database with chandoan data

--- src/main/java/com/mediaid/mediaid/repository/TrieuChungRepo.java
+++ src/main/java/com/mediaid/mediaid/repository/TrieuChungRepo.java
@@ -12,6 +12,9 @@ import java.util.List;
 public interface TrieuChungRepo extends JpaRepository<TrieuChung, String> {
     @Query(nativeQuery = true, value = "SELECT * from mediate_db.trieu_chung a order by CAST(SUBSTRING_INDEX(a.trieu_chungid, '_', -1) AS UNSIGNED) asc ")
     List<TrieuChung> findAllAndOrder();
-    @Query(nativeQuery = true, value = "select * from trieu_chung")
+    @Query(nativeQuery = true, value = "SELECT a.* FROM mediate_db.trieu_chung a " +
+            "right join mediate_db.trieu_chung_cua_bo_phan b " +
+            "on a.trieu_chungid = b.trieu_chungid " +
+            "where b.bo_phan_co_theid = ?2 AND a.status = ?1 ")
     List<TrieuChung> findByStatusAndBoPhanID(boolean status, String boPhanID);
 }
